170cd525eec470306e81b3f007c0679b7e5c8608
daffadrm <daffarayhan18@gmail.com>
2025-02-10 11:03:19 +0700

    feat: form designer

---
+++ form-designer.js
@@ -0,0 +1,26 @@
+module.exports = function (RED) {
+    function FormDesigner(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        
+        node.on('input', function (msg) {
+            try {
+                msg.files = config.files;
+                msg.rows = config.rows
+                node.warn("form designer: " + JSON.stringify(msg)); 
+                node.send(msg);
+            } catch (error) {
+                node.error("Invalid JSON", msg);
+            }
+        });
+    }
+
+    RED.nodes.registerType('form-designer', FormDesigner, {
+        settings: {
+            formDesignerColumns: { value: [], exportable: true }
+        },
+        defaults: {
+            formDesignerColumns: { value: [], required: false }
+        }
+    });
+};
